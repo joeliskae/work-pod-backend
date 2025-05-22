5dba893ee39033705e1f32893dcf45229e3e19fa
Joel Ryynänen <joel.ryynanen@gmail.com>
2025-05-22 14:23:52 +0300

    disabledn auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
-  //if (process.env.NODE_ENV === "development") return next();
+  if (process.env.NODE_ENV === "development") return next();
   if (req.isAuthenticated?.()) {
     console.log(`[Auth] user authenticated: ${(req.user as any)?.email || "unknown user"}`);
     return next();
