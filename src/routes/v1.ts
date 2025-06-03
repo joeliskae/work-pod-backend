e673fe8086374343ef008330fa958b00308fef26
Joel <joel.ryynanen@student.lab.fi>
2025-06-03 09:04:55 +0300

    Kalenteri route

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -9,11 +9,12 @@ import cancel from "./v1/cancel";
 import booking from "./v1/booking";
 import analytics from "./v1/analytics"
 
-// Devi endpoint, poista myöhemmin
-// import cache from "./v1/debug";
+// TODO: poista cache endpoint ennen tuotantoa!!!
+import cache from "./v1/debug";
 
 const router = Router();
 
+router.use(cache);
 router.use(book);
 router.use(busy);
 router.use(events);
@@ -24,6 +25,5 @@ router.use(booking);
 router.use(analytics);
 
 //Poista poista
-// router.use(cache);
 
 export default router;
