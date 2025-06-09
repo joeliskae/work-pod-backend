ffd06f599f2297eabda15f0153d06e217cb507d9
Joel <joel.ryynanen@student.lab.fi>
2025-06-09 13:16:00 +0300

    added new cors rules

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,8 @@ const allowedOrigins = [
   "http://localhost:3000", // esim. admin-panel
   "http://172.30.133.3:5173", // esim. devi servu
   "http://172.30.133.3:3000", // devi myös
+  "https://1d0e-193-166-177-58.ngrok-free.app/", //Oskarin devisäädöt
+  "172.30.132.212"
 ];
 
 async function startServer() {
